6889b0590256cfdac0571c0626cacece9f60755d
wuhongwei <hongwei.wu@grandsun.com>
2021-05-24 19:56:25 +0800

    xx

--- src/com/lullaby/pattern/StructureFlyWeightPattern.java
+++ src/com/lullaby/pattern/StructureFlyWeightPattern.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 /**
  * 享元模式
  *
- * 瓶盖的颜色是固定的几类，但是二维码信息是不同的
+ * 对象池的一种实现，目的是尽可能减少内存的使用。举个例子，瓶盖的颜色是固定的几类，但是二维码信息是不同的
  */
 public class StructureFlyWeightPattern {
 
@@ -18,8 +18,8 @@ public class StructureFlyWeightPattern {
         BottleCap bottleCap2 = BottleFlyWeightFactory.getBottleCap("蓝色");
 
         // 享元成员
-        System.out.println("zkh1" + bottleCap1 + "");
-        System.out.println("zkh2" + bottleCap2 + "");
+        System.out.println("bc1 hash: " + bottleCap1);
+        System.out.println("bc2 hash: " + bottleCap2);
 
         // 这里是对象有差异的元素
         System.out.println("获奖结果" + bottleCap1.getQrContent(new QRContent()));
@@ -27,7 +27,7 @@ public class StructureFlyWeightPattern {
     }
 
     static class BottleFlyWeightFactory {
-        //享元池
+
         private static final Map<String, BottleCap> map = new HashMap<>();
 
         public static BottleCap getBottleCap(String color) {
@@ -67,18 +67,17 @@ public class StructureFlyWeightPattern {
         }
     }
 
-    // 一般对象
     static class QRContent {
 
+        private final String uuid;
         private String qrContent;
-        private String uuid;
 
         public QRContent() {
             uuid = String.valueOf(UUID.randomUUID());
         }
 
         public String getQrContent() {
-            qrContent = "二维码唯一ID：" + uuid + "\n" + "根据ID得到中奖信息：" + (new Random().nextInt(6) + 1) + "等奖";
+            qrContent = "二维码唯一ID: " + uuid + "\n" + "根据ID得到中奖信息: " + (new Random().nextInt(6) + 1) + "等奖";
             return qrContent;
         }
 
